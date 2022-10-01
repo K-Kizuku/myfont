b17626f88c21005ae154b65d99fe5c17ab2eba38
yamato0211 <kumayama0211@gmail.com>
2022-10-02 02:06:31 +0900

    [add] herokuとdockerの併用

--- db/database.py
+++ db/database.py
@@ -4,18 +4,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 #DockerのDB設定
-# DATABASE = 'postgresql'
-# USER = os.environ.get('POSTGRES_USER')
-# PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-# DB_NAME = os.environ.get('POSTGRES_DB')
-# HOST = os.environ.get('POSTGRES_HOST')
-
-# SQLALCHEMY_DETABASE_URL = "{}://{}:{}@{}/{}".format(
-#     DATABASE, USER, PASSWORD, HOST, DB_NAME
-# )
-
 #herokuのdb設定
-SQLALCHEMY_DETABASE_URL = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
+if os.environ.get('DATABASE_URL') == None:
+    DATABASE = 'postgresql'
+    USER = os.environ.get('POSTGRES_USER')
+    PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    DB_NAME = os.environ.get('POSTGRES_DB')
+    HOST = os.environ.get('POSTGRES_HOST')
+
+    SQLALCHEMY_DETABASE_URL = "{}://{}:{}@{}/{}".format(
+        DATABASE, USER, PASSWORD, HOST, DB_NAME
+    )
+else: 
+    SQLALCHEMY_DETABASE_URL = os.environ.get(
+        'DATABASE_URL').replace('postgres://', 'postgresql://', 1)
 
 engine = create_engine(
     SQLALCHEMY_DETABASE_URL
