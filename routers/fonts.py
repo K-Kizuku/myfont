88c334ac97adc47a791cce4efc8f6534a69bec17
yamato0211 <kumayama0211@gmail.com>
2022-10-02 11:33:07 +0900

    [fix] 修正

--- routers/fonts.py
+++ routers/fonts.py
@@ -9,7 +9,9 @@ from schemas.users import Image
 font_router = APIRouter()
 
 @font_router.post("/", response_model=list[Image])
-async def create_font_images(fonts_images: FontPayload, user_id: str = Depends(get_current_user), db: Session = Depends(get_db)):
+def create_font_images(fonts_images: FontPayload, user_id: str = Depends(get_current_user), db: Session = Depends(get_db)):
+    print(fonts_images)
+    print(fonts_images == None)
     if user_id is None:
         raise HTTPException(status_code=403, detail="jwt_token is invalid!")
     images = insert_images(db, fonts_images.images, user_id)
